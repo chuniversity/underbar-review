b2cdd8693f43669a70153a583c194e606f69f3e6
Charles Fowler <chas.r.fowler@gmail.com>
2021-04-27 11:57:26 -0500

    Iomplemented code for spec/part1 and src/underbar for filter and reject

--- src/underbar.js
+++ src/underbar.js
@@ -84,12 +84,26 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    _.each(collection, function(item) {
+      if (test(item) === true) {
+        result.push(item);
+      }
+    });
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var result = [];
+    _.filter(collection, function(item) {
+      if (test(item) === false) {
+        result.push(item);
+      }
+    });
+    return result;
   };
 
   // Produce a duplicate-free version of the array.
